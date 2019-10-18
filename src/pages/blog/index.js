2d1f0af685e2e18121d224376fce21d12e762bdf
Christian Henry <chenry5724@gmail.com>
2019-10-17 22:11:59 -0500

    delete extraneous2

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
+// import React from 'react'
 
-import Layout from '../../component/Layout'
-import BlogRoll from '../../component/BlogRoll'
+// import Layout from '../../component/Layout'
+// import BlogRoll from '../../component/BlogRoll'
 
-export default class BlogIndexPage extends React.Component {
-  render() {
-    return (
-      <Layout style={{backgroundColor:'rgba(254, 247, 230, 0.87)', height:'100vh'}}>
+// export default class BlogIndexPage extends React.Component {
+//   render() {
+//     return (
+//       <Layout style={{backgroundColor:'rgba(254, 247, 230, 0.87)', height:'100vh'}}>
         {/* <div
           className="full-width-image-container margin-top-0"
           style={{
@@ -14,28 +14,28 @@ export default class BlogIndexPage extends React.Component {
             backgroundImage: `url('/img/blog-index.jpg')`,
           }}
         > */}
-          <h1
-            className="has-text-weight-bold is-size-1"
-            style={{
+          // <h1
+          //   className="has-text-weight-bold is-size-1"
+          //   style={{
               // boxShadow: '0.5rem 0 0 rgba(30, 26, 26, 0.79), -0.5rem 0 0 rgba(30, 26, 26, 0.79)',
-              backgroundColor: 'rgba(30, 26, 26, 0.79)',
-              color: 'white',
-              padding: '1rem',
-              textAlign: 'center',
-              marginTop:'0em',
-            }}
-          >
-            Current Projects
-          </h1>
-        {/* </div> */}
-        <section className="section" style={{backgroundColor:'rgba(254, 247, 230, 0.87)', height:'100%'}}>
-          <div className="container">
-            <div className="content">
-              <BlogRoll />
-            </div>
-          </div>
-        </section>
-      </Layout>
-    )
-  }
-}
+//               backgroundColor: 'rgba(30, 26, 26, 0.79)',
+//               color: 'white',
+//               padding: '1rem',
+//               textAlign: 'center',
+//               marginTop:'0em',
+//             }}
+//           >
+//             Current Projects
+//           </h1>
+//         {/* </div> */}
+//         <section className="section" style={{backgroundColor:'rgba(254, 247, 230, 0.87)', height:'100%'}}>
+//           <div className="container">
+//             <div className="content">
+//               <BlogRoll />
+//             </div>
+//           </div>
+//         </section>
+//       </Layout>
+//     )
+//   }
+// }
